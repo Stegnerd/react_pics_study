3fcaf89a95c96daee437fad7a6d751534f38fd2a
dstegner <dmstegner@gmail.com>
2019-02-16 00:20:17 -0500

    passed props to children

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,15 +2,37 @@ import React from "react";
 
 class SearchBar extends React.Component {
   // by leaving off paranthesis so it is a callback, only called for change not render
-  onInputChange(event) {}
+  // uncontrolled handler
+  // onInputChange(event) {
+  //   console.log(event.target.value);
+  // }
+
+  state = { term: "" };
+
+  // arrow functions help keep context of state
+  onFormSubmit = event => {
+    event.preventDefault();
+
+    this.props.onSubmit(this.state.term);
+  };
 
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        {/* this onsubmit is not the same as the prop that was passed in */}
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type="text" onChange={this.onInputChange} />
+            {/* uncontrolled way to handle the event */}
+            {/* <input type="text" onChange={this.onInputChange} /> */}
+            {/* controlled way to handle the event */}
+            <input
+              type="text"
+              onChange={e =>
+                this.setState({ term: e.target.value.toUpperCase() })
+              }
+              value={this.state.term}
+            />
           </div>
         </form>
       </div>
